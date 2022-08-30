969f6762834065dda130e3d6a0f017092106de1a
Alex <alexvkleschov@gmail.com>
2022-08-30 08:48:20 +0400

    device parser

--- auth_api/src/core/utils.py
+++ auth_api/src/core/utils.py
@@ -1,4 +1,26 @@
 import datetime as dt
+from user_agents import parse
+from enum import Enum
+
+
+class Device(Enum):
+    WEB = 'Браузер'
+    MOBILE = 'Телефон'
+    SMART = 'Смарт-ТВ'
+
+
+def useragent_device_parser(useragent: str) -> Device | None:
+    """Считывает устройство из строки Юзер-Агента"""
+    user_agent = parse(useragent)
+    # Парсим устройства
+    if str(user_agent).find("TV") != -1:
+        return Device.SMART
+    if user_agent.is_mobile or user_agent.is_tablet:
+        return Device.MOBILE
+    if user_agent.is_pc:
+        return Device.WEB
+
+    return None
 
 
 def get_unix_timedelta(unix_time: str | dt.datetime | int):
